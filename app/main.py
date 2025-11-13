d02481646625873e80c54a7a6bca92644ff41e3c
Ievgen German <german.kbrza@gmail.com>
2025-11-13 20:22:22 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -2,11 +2,37 @@ from abc import ABC
 
 
 class IntegerRange:
-    pass
+    def __init__(
+            self,
+            min_amount: int,
+            max_amount: int) -> None:
+        self.min_amount = min_amount
+        self.max_amount = max_amount
+
+    def __set_name__(
+            self,
+            owner: type,
+            name: str) -> None:
+        self.protected_name = "_" + name
+
+    def __get__(
+            self,
+            instance: None | object,
+            owner: type):
+        return getattr(instance, self.protected_name)
+
+    def __set__(
+            self,
+            instance: None | object,
+            value: int) -> int:
+        if not isinstance(value, int):
+            raise TypeError()
+        if self.min
+
+    class Visitor:
 
 
-class Visitor:
-   pass
+pass
 
 
 class SlideLimitationValidator(ABC):
